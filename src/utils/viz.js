7b51f0629b45ff65dac1022cf4a77562590ba3d9
petercolella <peter.joseph.colella@gmail.com>
2021-06-17 11:47:51 -0400

    updates getData.js and viz.js with different data endpoint and options, respectively

--- src/utils/viz.js
+++ src/utils/viz.js
@@ -1,10 +1,8 @@
 import * as vl from "vega-lite-api";
 export const viz = vl
-  .markCircle({ size: 300, opacity: 0.5 })
+  .markLine({ size: 5, opacity: 1 })
   .encode(
-    vl.x().fieldQ("mpg").scale({ zero: false }),
-    vl.y().fieldQ("horsepower").scale({ zero: false }),
-    vl.color().fieldN("origin"),
-    vl.size().fieldQ("weight"),
-    vl.tooltip().fieldN("name")
+    vl.x().fieldT("timestamp").scale({ zero: false }),
+    vl.y().fieldQ("temperature").scale({ zero: false }),
+    vl.tooltip().fieldN("temperature")
   );
